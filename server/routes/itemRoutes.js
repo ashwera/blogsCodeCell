7033c3807cc9ed9512f3bcf099806d3c80b57713
omikacharya <omikacharya@gmail.com>
2026-05-23 21:36:40 +0530

    chore: remove redundant backend and old client

--- server/routes/itemRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getItems, setItem } = require('../controllers/itemController');
-const { protect } = require('../middleware/authMiddleware');
-
-router.route('/').get(getItems).post(protect, setItem);
-
-module.exports = router;
